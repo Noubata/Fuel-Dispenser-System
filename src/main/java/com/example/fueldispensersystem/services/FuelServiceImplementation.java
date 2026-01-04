a6458c9f933cec9da2462853dce0dcc9a3034212
Beny <benynouba@gmail.com>
2026-01-04 07:12:45 +0100

    a multi dispenser fuel system

--- src/main/java/com/example/fueldispensersystem/services/FuelServiceImplementation.java
+++ src/main/java/com/example/fueldispensersystem/services/FuelServiceImplementation.java
@@ -2,9 +2,13 @@ package com.example.fueldispensersystem.services;
 
 import com.example.fueldispensersystem.dtos.responses.GetAvailableFuelResponse;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class FuelServiceImplementation implements FuelService {
     @Override
     public GetAvailableFuelResponse getAvailableFuel() {
-
+        List<HashMap> dispensers =  new ArrayList<>();
     }
 }
